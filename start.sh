8cb01f20a1328a32264891de9271e75bd41776cd
Razel1 <83173027+Razel1@users.noreply.github.com>
2026-07-09 15:50:06 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 1. Jalankan Backend Python di background (&)
-echo "Menjalankan Backend Python..."
-python api_klinik.py &
-
-# 2. Jalankan Frontend Node.js (biarkan jalan di foreground)
-echo "Menjalankan Frontend Node..."
-node .output/server/index.mjs
